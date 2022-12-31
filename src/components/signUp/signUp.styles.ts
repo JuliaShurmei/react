d1ba27ad84a896e325c655416e178b728a0ab3c8
JuliaShurmei <ivanovaulia428@gmail.com>
2022-12-31 12:00:00 +0300

    Change theme

--- src/components/signUp/signUp.styles.ts
+++ src/components/signUp/signUp.styles.ts
@@ -2,12 +2,3 @@ interface SignUpStylesType {
   MainContainer: any;
 }
 
-export const SignUpStyles: SignUpStylesType = {
-  MainContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    gap: 20,
-  },
-};
